365af1036766efb9fa1309c5ee11fdc29033304d
Panchada Vamsi <panchada@crayondata.com>
2023-10-11 16:25:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
 import './App.css';
+import Login from './components/login';
+import Register from './components/request';
+import RegisterResonse from './components/RegisterResponse';
+
+
+const handleDragStart = (e) => e.preventDefault();
+
+const items = [
+  <img src="img1.png" className='imgsrc' onDragStart={handleDragStart} role='show' alt='img1'/>,
+  <img src="img2.png" className='imgsrc' onDragStart={handleDragStart} role="show" alt='img2'/>,
+  <img src="img3.png" className='imgsrc' onDragStart={handleDragStart} role="show" alt='img3'/>,
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='sliderimg'>
+        <AliceCarousel mouseTracking items={items} autoPlay autoPlayInterval={2000} disableButtonsControls='true' infinite/>
+      </div>
+      <div className='leftpart'>
+        {/* <p><Register /></p> */}
+        {/* <p><Login /></p> */}
+        <p><RegisterResonse /></p>
+      
+      </div>
     </div>
   );
 }
